627520e6d00219ec9ba314a7499bccfffbacb22a
Rotos Aries <rotosaries@gmail.com>
2025-03-16 23:29:36 +0800

    添加Element Plus及其图标库并集成到项目中

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,23 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import "element-plus/dist/index.css";
 
 // 设置axios的全局默认配置
 axios.defaults.baseURL = "http://localhost:8000/api";
 
 const app = createApp(App);
+
+// 注册Element Plus
+app.use(ElementPlus);
+
+// 注册Element Plus图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
 app.config.globalProperties.$axios = axios;
 app.use(router);
 app.mount("#app");
